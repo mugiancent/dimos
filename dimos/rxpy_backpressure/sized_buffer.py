9010d92b2c2b02a5e2046d8c77c610ec4c9ab534
Paul Nechifor <paul@nechifor.net>
2026-01-06 04:02:21 +0000

    fix imports
    Former-commit-id: e8e4a7e75c86269532599682b9e5b6945cc8ba3c [formerly 38fcbf75d6d713e09093d7f63c3496c13d2f8fc8] [formerly bda3bf36e10e88bc5bb2fff17457651ceac846a3 [formerly 458120b4cc48865232d380315396aac0987bfb9b]]
    Former-commit-id: 7fd287d4329788047ceb4ff6603831ecd0f53be4
    Former-commit-id: 1cdfb1cae6a602087a2766eed9170bb2a317655c

--- dimos/rxpy_backpressure/sized_buffer.py
+++ dimos/rxpy_backpressure/sized_buffer.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, List, Optional
+from typing import Any
 
 from rxpy_backpressure.function_runner import thread_function_runner
 from rxpy_backpressure.locks import BooleanLock, Lock
